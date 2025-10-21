e752efae0b7e8d4a687cab246ec8ee8a70fffcd0
python-fuse <hauwamuktar880@gmail.com>
2025-10-21 12:55:06 +0100

    feat: implemented about and contact page and navigation

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-let time = Date.now();
+const pageToRoute = {
+  Home: "/index.html",
+  "About Me": "/about.html",
+  "Contact Us": "/contact.html",
+};
 
-function updateTime() {
-  time = Date.now();
-  timeElement.innerHTML = time;
+function highlightCurrentPage() {
+  // Grab the the links in header>nav>ul>li and highlight current page
+  let navLinks = document.querySelectorAll("header>nav>ul>li a");
+  const currentPage = window.location.pathname;
 
-  console.log("hi");
+  navLinks.forEach((link) => {
+    if (pageToRoute[link.innerHTML] === currentPage) {
+      link.classList.add("active-link");
+    }
+  });
 }
 
-setInterval(updateTime, 1000);
-
-let timeElement = document.getElementById("time");
+highlightCurrentPage();
